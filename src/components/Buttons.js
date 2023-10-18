a460c600883a2f3ee5bbcf5fd79e5da34fda61ff
Tanmayjain0906 <tanmayjain983@gmail.com>
2023-10-18 19:06:26 +0530

    menu

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -6,10 +6,10 @@ const Buttons = ({setCat}) => {
 
     return (
         <div class="buttons">
-            <button onClick={() => setCat("All")}>All</button>
-            <button onClick={() => setCat("breakfast")}>Breakfast</button>
-            <button onClick={() => setCat("lunch")}>Lunch</button>
-            <button onClick={() => setCat("shakes")}>Shakes</button>
+            <button onClick={() => setCat("All")} id="main">All</button>
+            <button onClick={() => setCat("breakfast")} id="filter-btn-1">Breakfast</button>
+            <button onClick={() => setCat("lunch")} id="filter-btn-2">Lunch</button>
+            <button onClick={() => setCat("shakes")} id="filter-btn-3">Shakes</button>
         </div>
     )
 }
